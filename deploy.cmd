c78ebf5361dc0a25f8eeee053b90c53bb2699975
Vitaly Goncharenko <vitaliy.goncharenko@hotmail.com>
2017-08-28 19:05:59 +0800

    [PowerBIAngularClient] fix deploy

--- deploy.cmd
+++ deploy.cmd
@@ -114,7 +114,7 @@ call :ExecuteCmd !NPM_CMD! install typescript -g
 echo Installing Yarn Packages.
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd yarn install --production
+  call :ExecuteCmd yarn install
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
